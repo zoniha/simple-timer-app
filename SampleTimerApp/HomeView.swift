902c5d06a19629207647ee6108df02347a3a1f1e
zoniha <awaawapurin@gmail.com>
2021-08-17 16:09:36 +0900

    🎉 init: Initial commit

---
+++ SampleTimerApp/HomeView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
--- SampleTimerApp/SampleTimerAppApp.swift
+++ SampleTimerApp/SampleTimerAppApp.swift
@@ -1,17 +1,10 @@
-//
-//  SampleTimerAppApp.swift
-//  SampleTimerApp
-//
-//  Created by zoniha on 2021/08/17.
-//
-
 import SwiftUI
 
 @main
 struct SampleTimerAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
