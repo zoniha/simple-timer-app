f831b4a190f6b6c6f0f363eed20fd992bf5fcf94
zoniha <awaawapurin@gmail.com>
2021-08-17 15:59:13 +0900

    Initial Commit

---
+++ SampleTimerApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleTimerApp
+//
+//  Created by zoniha on 2021/08/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SampleTimerApp/SampleTimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleTimerAppApp.swift
+//  SampleTimerApp
+//
+//  Created by zoniha on 2021/08/17.
+//
+
+import SwiftUI
+
+@main
+struct SampleTimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
