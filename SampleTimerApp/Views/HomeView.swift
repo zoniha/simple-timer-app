c0904cd9e2840e3b23f501cc17f5897b091e138d
zoniha <awaawapurin@gmail.com>
2021-08-18 17:16:53 +0900

    🎨layout: Add some buttons

--- SampleTimerApp/Views/HomeView.swift
+++ SampleTimerApp/Views/HomeView.swift
@@ -1,24 +1,48 @@
 import SwiftUI
 
 struct HomeView: View {
-    var body: some View {
+	var body: some View {
 		NavigationView {
 			ZStack {
 				Image("wallpaper")
 					.resizable()
 					.edgesIgnoringSafeArea(.all)
 					.aspectRatio(contentMode: .fill)
+				VStack(spacing: 30.0) {
+					Text("残り〇〇秒")
+						.font(.largeTitle)
+						.foregroundColor(.white)
+					HStack {
+						Button(action: {}, label: {
+							Text("スタート")
+								.font(.title)
+								.foregroundColor(.white)
+								.frame(width: 140, height: 140)
+								.background(Color("startColor"))
+								.clipShape(Circle())
+						})
+						Button(action: {}, label: {
+							Text("ストップ")
+								.font(.title)
+								.foregroundColor(.white)
+								.frame(width: 140, height: 140)
+								.background(Color("stopColor"))
+								.clipShape(Circle())
+						})
+					}
+				}
 			}
 			.navigationBarItems(trailing: NavigationLink(destination: SettingView()) {
 				Text("秒数設定")
-									}
+					.foregroundColor(.white)
+			}
 			)
 		}
 	}
 }
 
 struct HomeView_Previews: PreviewProvider {
-    static var previews: some View {
-        HomeView()
-    }
+	static var previews: some View {
+		HomeView()
+	}
 }
