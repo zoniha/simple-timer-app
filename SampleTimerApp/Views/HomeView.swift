260f94db573491973ef094bf162a24004bd4073e
zoniha <awaawapurin@gmail.com>
2021-08-18 16:58:41 +0900

    🎨 layout: Set background image

--- SampleTimerApp/Views/HomeView.swift
+++ SampleTimerApp/Views/HomeView.swift
@@ -2,9 +2,19 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+		NavigationView {
+			ZStack {
+				Image("wallpaper")
+					.resizable()
+					.edgesIgnoringSafeArea(.all)
+					.aspectRatio(contentMode: .fill)
+			}
+			.navigationBarItems(trailing: NavigationLink(destination: SettingView()) {
+				Text("秒数設定")
+									}
+			)
+		}
+	}
 }
 
 struct HomeView_Previews: PreviewProvider {
