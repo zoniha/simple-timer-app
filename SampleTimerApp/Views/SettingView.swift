4416ff9bdf7b0eaab4b6851132ffbd6cfe460866
zoniha <awaawapurin@gmail.com>
2021-08-18 17:33:27 +0900

    🎨 layout: Update

--- SampleTimerApp/Views/SettingView.swift
+++ SampleTimerApp/Views/SettingView.swift
@@ -1,13 +1,48 @@
 import SwiftUI
 
 struct SettingView: View {
-    var body: some View {
-        Text("")
-    }
+	@State var timerValue = 10
+
+	var body: some View {
+		ZStack {
+			Image("wallpaper")
+				.resizable()
+				.edgesIgnoringSafeArea(.all)
+				.aspectRatio(contentMode: .fill)
+			VStack {
+				Spacer()
+				Text("\(timerValue)秒")
+					.font(.largeTitle)
+					.foregroundColor(.white)
+				Spacer()
+				Picker(selection: $timerValue, label: Text("選択")) {
+					Text("10")
+						.tag(10)
+						.foregroundColor(.white)
+					Text("20")
+						.tag(20)
+						.foregroundColor(.white)
+					Text("30")
+						.tag(300)
+						.foregroundColor(.white)
+					Text("40")
+						.tag(40)
+						.foregroundColor(.white)
+					Text("50")
+						.tag(50)
+						.foregroundColor(.white)
+					Text("60")
+						.tag(60)
+						.foregroundColor(.white)
+				}
+				Spacer()
+			}
+		}
+	}
 }
 
 struct SettingView_Previews: PreviewProvider {
-    static var previews: some View {
-        SettingView()
-    }
+	static var previews: some View {
+		SettingView()
+	}
 }
